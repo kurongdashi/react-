9612e2f196c2430c1cad526edca9ecfed9829ac6
lp123 <1970664102@qq.com>
2024-05-06 22:36:14 +0800

    tijiao

--- src/test.js
+++ src/test.js
@@ -1,6 +1,28 @@
-console.log(1);
+console.log(1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function test(a, b) {
-  return a + b;
+    return a + b
 }
-console.log(1);
-console.log(2);
+console.log(1)
+
+
+
+
+
+
+
+
+console.log(2)
